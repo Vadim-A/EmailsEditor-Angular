bb84b330d1766a8efc3ecd06426ba43a2a386c39
Vadim-A <golovachyov@gmail.com>
2016-07-23 21:45:44 +0500

    refactoring

--- EmailsEditor/EmailsEditor/js/EmailsEditor.js
+++ EmailsEditor/EmailsEditor/js/EmailsEditor.js
@@ -1,156 +1,117 @@
-﻿var app = angular.module('EmailModule', [])
+﻿//About business logic in ng-app
+//https://www.codeschool.com/discuss/t/angularjs-should-business-logic-go-into-controller-or-link-in-custom-directives/4821/2
+//http://stackoverflow.com/questions/14480018/app-architecture-directives-vs-controllers
+
+var app = angular.module('EmailModule', [])
 
 app.directive('emailsEditor', function () {
-
-	var _emails = [
-		{ id: 1, email: 'sidorov@mail.ru' },
-		{ id: 2, email: 'popov@mail.ru' }
-	];
-	
-	function _addEmail(email) {
-		_emails.push({
-			id: _emails[_emails.length - 1].id + 1,
-			email: email
-		});
-	};
-
-	function _delEmail(email) {
-		_emails.splice(_emails.indexOf(email), 1);
-	}
-
-	function _validateEmail(email) {
-		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		return re.test(email);
-	}
-
-	function delEmail() {
-		_delEmail(this.email);
-	};
-
-	function onEmailEdit() {
-		//NOTE: Через ng-keypress и event нельзя определить новое и старое значение.
-		//А так же сложно определить сочетание клавишь (будет ли event.keyCode на другой раскладке таким же?)
-		//if (event.ctrlKey && (event.keyCode == 86 || event.keyCode == 87)) {
-		//}
-		
-		if (this.newEmail != null && this.newEmail.length > 0 && (event.keyCode == 13 || event.key == ',')) {
-			_addEmail(this.newEmail);
-			this.newEmail = "";
-			event.preventDefault();
-		}
-	};
-
-	function onBlur() {
-		if (this.newEmail != null && this.newEmail.length > 0) {
-			_addEmail(this.newEmail);
-			this.newEmail = "";
-		}
-	};
-
-	function onEmailPaste() {
-		this.emailPaste = true;
-	}
-
-	function getBadEmailClass() {
-		return _validateEmail(this.email.email) ? '' : 'badEmail';
-	};
-	
+	//There is only template and functions whitch strongly connected with template in the directive
 
 	return {
-		restrict: 'E',
+		restrict: 'E', //can be used only like element "<emails-editor></emails-editor>"
+		replace: true,//means that directive in html ("<emails-editor></emails-editor>") will be replased by template
 
-		template: 
+		//https://toddmotto.com/dynamic-controllers-in-directives-with-the-undocumented-name-property/
+		name: 'ctrl', //controller name...
+		controller: '@', //...from attribute
+
+		template: //this content will bi in html instead "<emails-editor></emails-editor>"
 '<div id="emails-editor">' + 
 '<ul class="email">' +
-	'<li ng-repeat="email in emails"><span class={{getBadEmailClass()}}>' + //NOTE: ng-class не применяет стили
-		'{{email.email}}</span><span class="remove" ng-click="delEmail()">x</span></li>' +
+	'<li ng-repeat="email in _emails"><span class={{getBadEmailClass()}}>' + //NOTE: using "ng-class" styles does not apply. What wrong?
+		'{{email}}</span><span class="remove" ng-click="delEmail(email)">x</span></li>' +
 '</ul>' +
-'<textarea ng-blur="onBlur()" ng-paste="onEmailPaste()" ng-keypress="onEmailEdit()" wrap="off" ng-model="newEmail" row="1" class="emailInput" placeholder="add more people..."></textarea>' +
-//'<textarea ng-paste="expression" ng-keydown="onEmailEdit()" wrap="off" ng-model="newEmail" row="1" class="emailInput" placeholder="add more people..."></textarea>' +
+'<textarea ng-blur="onBlur()" ng-paste="onPaste()" ng-keypress="onEmailEdit()" wrap="off" ng-model="newEmail" row="1" class="emailInput" placeholder="add more people..."></textarea>' +
 '</div>',
 
-		replace: true,//http://stackoverflow.com/questions/15285635/how-to-use-replace-of-directive-definition
-
 		link: function (scope, element, attrs) {
-			scope.emails = _emails; //список почтовых ящиков
-			scope.getBadEmailClass = getBadEmailClass; // подчеркивание почтового ящика, не соответствующего шаблону
-			scope.delEmail = delEmail; // удаление блока почтового ящика из списка
-			scope.onEmailEdit = onEmailEdit; //при редактировании поля с почтовым ящиком необходмо сформировать блок на нажатие ',' и 'enter'
-			scope.onBlur = onBlur; //действие при потере фокуса 
 
-			scope._addEmail = _addEmail; //for controller
+			scope.getBadEmailClass = function () {//return class name for bad emails
+				return this._validateEmail(this.email) ? '' : 'badEmail';
+			};
+
+			scope.onEmailEdit = function () {
+				//Using in "ng-keypress" event. 
+				//Does not used to catch "ctrl+v" event because it is difficult to determine
+				//keyCode for "V" key keeping in mind the multilanguage OS.
+				//For "ctrl+v" used "ng-paste" event
+				//TODO: check event for "ctrl+v" event
+				if (event.keyCode == 13 || event.key == ',') { //"Enter" or "Comma"
+					this.addEmail(this.newEmail);
+					this.newEmail = "";
+					event.preventDefault();
+				}
+			};
+
+			//Add email on lost focus
+			scope.onBlur = function () {
+				if (this.newEmail != null && this.newEmail.length > 0) {
+					this.addEmail(this.newEmail);
+					this.newEmail = "";
+				}
+			};
 			
-
-			//формирование блоков почтовых ящиков при ctrl+v обрабатывается отдельно
-			scope.onEmailPaste = onEmailPaste;
+			
 			scope.emailPaste = false;
+			scope.onPaste = function (newEmail) {
+				//"ng-paste" event does not alloy to get paste values, but scope.$watch does.
+				//In "ng-paste" event used variable to fix that "paste" event is happened
+				this.emailPaste = true;
+			};
 			scope.$watch("newEmail", function (newVal, oldVal, scope) {
-				if (scope.emailPaste) {
+				//Cannot determine "Enter" key in listener 
+				if (scope.emailPaste) { //only when "paste" event is happened
 					newVal = newVal.split('\n').join(',');
 					newVal = newVal.split('\t').join(',');
 					newVal = newVal.split(' ').join(',');
-					newVal.split(',').forEach(function (element, index, array) {
-						if (element.length > 0) {
-							_addEmail(element);
-						}
-					});
+					scope.addEmails(newVal.split(','));
 					scope.newEmail = "";
 					scope.emailPaste = false;
 				}
 			});
 		
 		},
-
-		
-		//https://toddmotto.com/dynamic-controllers-in-directives-with-the-undocumented-name-property/
-		name: 'ctrl', //controller name...
-		controller: '@' //...from attribute
-		
 	}
 })
 
 app.controller("controller", function ($scope) {
 
+	$scope._emails = ['sidorov@mail.ru', 'popov@mail.ru'];
+	
+	$scope._validateEmail = function (email) {
+		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(email);
+	};
 
-	$scope.addEmails = function () {
-		//while (this.emails.indexOf(email) != -1) {
-			var email = "email" + Math.round(Math.random() * 1000) + "@mail.ru";
-		//};
-			this._addEmail(email);
-	}
+	$scope.addEmail = function (email) {
+		if (email && email.length > 0 && this._emails.indexOf(email) == -1) {
+			this._emails.push(email);
+		}
+	};
+
+	$scope.delEmail = function (email) {
+		this._emails.splice(this._emails.indexOf(email), 1);
+	};
+
+	$scope.addEmails = function (emails) {
+		if (emails) {
+			emails.forEach(function (element, index, array) {
+				this.addEmail(element);
+			}, this); //NOTE: [this] argument for input current context (scope) into "forEach"
+		} else {
+			var email;
+			do {
+				var email = "email" + Math.round(Math.random() * 1000) + "@mail.ru";
+			} while (this._emails.indexOf(email) != -1);
+			this.addEmail(email);
+		}
+	};
 	
 	$scope.getEmailsCount = function () {
-		alert(this.emails.length);
-	}
+		alert(this._emails.length);
+	};
 	
+	$scope.getEmails = function () {
+		return this._emails;
+	};
 })
-
-
-/*
-angular.module('moduleName', [])
-    .directive('directiveName', function () {
-        return {
-             compile: function compile(temaplateElement, templateAttrs) {
-                return {
-                    pre: function (scope, element, attrs) {
-                    },
-                    post: function(scope, element, attrs) { 
-                    }
-                }
-            },
-            link: function (scope, element, attrs) {
-
-            },
-            priority: 0,
-            terminal:false,
-            template: '<div></div>',
-            templateUrl: 'template.html',
-            replace: false,
-            transclude: false,
-            restrict: 'A',
-            scope: false,
-            controller: function ($scope, $element, $attrs, $transclude, otherInjectables) {
-            }           
-        }
-    });
-	*/
